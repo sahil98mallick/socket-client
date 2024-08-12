9a7be76a4ebcbb0fbe0262cfce8cd2f25c6a6fea
sahil98mallick <sahil98mallick@gmail.com>
2024-08-12 21:53:39 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://socket-server-flax.vercel.app");
 
 const App: React.FC = () => {
   const [message, setMessage] = useState("");
